499daabe9a10fb4b46a60bd69890f3f26af67b83
phdfreitas <pedro.hnd.freitas@gmail.com>
2022-07-11 18:39:59 -0300

    First Component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,11 @@
+import NotificationButton from "./components/notification-button"
+
 function App() {
   return(
-    <h1>Hello World</h1>
+    <>
+      <h1>Hello World</h1>
+      <NotificationButton/>
+    </>
   )
 }
 
